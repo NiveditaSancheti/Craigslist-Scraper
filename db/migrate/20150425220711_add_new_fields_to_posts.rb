7477d2aff060d019788d4f638e8d64c1c422d249
NiveditaSancheti <nivedita.sancheti@yahoo.com>
2015-05-06 03:22:03 -0700

    heroku

--- db/migrate/20150425220711_add_new_fields_to_posts.rb
+++ db/migrate/20150425220711_add_new_fields_to_posts.rb
@@ -2,7 +2,6 @@ class AddNewFieldsToPosts < ActiveRecord::Migration
   def change
     add_column :posts, :user_rating, :integer
     add_column :posts, :created_timestamp, :datetime
-    add_column :posts, :created_date, :string
     add_column :posts, :created_month, :string
     add_column :posts, :created_year, :integer
     add_column :posts, :created_day, :integer
